3e767b08a060970f3149e1563ef751fecc3e6a07
dcruzcruz <dcruzcruz@academic.rrc.ca>
2024-02-27 20:42:18 -0600

    We have added the about page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+# app/controllers/pages_controller.rb
+class PagesController < ApplicationController
+  def about
+    # Add any necessary logic here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'about', to: 'pages#about'
 
   resources :accounts
   #resources :transactions
