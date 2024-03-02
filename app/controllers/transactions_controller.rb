6e5167e80ba3af46285ff22aa6adc57a4635efc6
dcruzcruz <dcruzcruz@academic.rrc.ca>
2024-03-02 11:44:24 -0600

    Succesfully implemented  the hierarchical search feature

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,12 +2,25 @@ class TransactionsController < ApplicationController
   before_action :set_transaction, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:search].present?
-      @transactions = Transaction.where('description LIKE ?', "%#{params[:search]}%")
+    if params[:search].present? || params[:category].present?
+      transactions = Transaction.includes(:categories)
+
+      if params[:search].present?
+        transactions = transactions.where('transactions.description LIKE ?', "%#{params[:search]}%")
+      end
+
+      if params[:category].present?
+        transactions = transactions.where('categories.id = ?', params[:category])
+      end
+
+      @transactions = transactions.references(:categories)
     else
       @transactions = Transaction.page(params[:page]).per(10)
     end
+
+    @categories = Category.all # Fetch all categories for the dropdown
   end
+
 
   def show
     # set_transaction before_action will set the @transaction instance variable
--- db/migrate/20240223215220_create_transaction_categories.rb
+++ db/migrate/20240223215220_create_transaction_categories.rb
@@ -1,10 +1,13 @@
 class CreateTransactionCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :transaction_categories do |t|
-      t.belongs_to :transaction, index: true, foreign_key: true
-      t.belongs_to :category, index: true, foreign_key: true
+      t.integer :transaction_id
+      t.integer :category_id
 
       t.timestamps
     end
+
+    add_foreign_key :transaction_categories, :transactions
+    add_foreign_key :transaction_categories, :categories
   end
 end
